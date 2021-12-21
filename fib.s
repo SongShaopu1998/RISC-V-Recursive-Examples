2d2d1e2ab119ecc3d46cb4f54869fb9ecd9db0fb
shaopu Song <songshaopu1998@gmail.com>
2021-12-21 12:12:18 +0800

    Update fib.s

--- fib.s
+++ fib.s
@@ -16,7 +16,7 @@ main:
     addi a0, x0, 10
     ecall # terminate ecall
 fib:
-	addi sp, sp, -8
+    addi sp, sp, -8
     # store the return address & current n
     sw ra, 0(sp)
     # save the current N's copy
@@ -30,14 +30,15 @@ fib:
     jr ra
 
 else:
-	# n - 1
-	addi a0, a0, -1
+    # n - 1
+    addi a0, a0, -1
     # call fib(n - 1)
     jal fib
+    
     # to calculate n - 2, we must save a copy of N before
     # and we also need to save the current result fib(n - 1)
     # t0 is n
-	lw t0, 4(sp)
+    lw t0, 4(sp)
     # a0 stores the current result
     sw a0, 4(sp)
     # n - 2
